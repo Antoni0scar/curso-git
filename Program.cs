47e2d0cd2e577e7095f60e7c3abc178b1528b315
Antonio Oscar <antoni0scar@gmail.com>
2020-09-14 07:08:51 -0300

    Acrescentada a pergunta sobre como voce estah

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ namespace ControleDeVersoes {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("OlaMundo");
-
             Console.WriteLine("Correção");
+            Console.WriteLine("Como você está?");
+
+            Console.ReadLine();
 
         }
     }
